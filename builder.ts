7a0b205dd0c06e70277a775ea29560e0eb5a646b
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-13 18:46:32 +0800

    Fix builder again

--- builder.ts
+++ builder.ts
@@ -1,8 +1,10 @@
+import { dirname as pathDirName } from "https://deno.land/std@0.208.0/path/dirname.ts";
 import { join as pathJoin } from "https://deno.land/std@0.208.0/path/join.ts";
 import links from "./links.ts";
 const root: string = pathJoin(Deno.cwd(), "_site");
 await Deno.mkdir(root, { recursive: true });
-for (const [path, url] of links.entries()) {
-	const file: Deno.FsFile = await Deno.create(pathJoin(root, path, "index.html"));
-	await file.write(new TextEncoder().encode(`<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=${url}"></head><body></body></html>`));
+for (const [pathRelative, url] of links.entries()) {
+	const pathFull: string = pathJoin(root, pathRelative, "index.html");
+	await Deno.mkdir(pathDirName(pathFull), { recursive: true });
+	await Deno.writeFile(pathFull, new TextEncoder().encode(`<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=${url}"></head><body></body></html>`));
 }
