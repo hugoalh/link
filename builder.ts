f4763f6c1c3e271646c60f73e302a6fa235a4e0f
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-13 18:42:48 +0800

    Fix builder

--- builder.ts
+++ builder.ts
@@ -3,5 +3,6 @@ import links from "./links.ts";
 const root: string = pathJoin(Deno.cwd(), "_site");
 await Deno.mkdir(root, { recursive: true });
 for (const [path, url] of links.entries()) {
-	await Deno.writeFile(pathJoin(root, path, "index.html"), new TextEncoder().encode(`<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=${url}"></head><body></body></html>`));
+	const file: Deno.FsFile = await Deno.create(pathJoin(root, path, "index.html"));
+	await file.write(new TextEncoder().encode(`<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=${url}"></head><body></body></html>`));
 }
