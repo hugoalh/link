3a2696f35f403db4a89b97f050644b1b808c2ce2
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-13 18:37:58 +0800

    Initial

---
+++ builder.ts
@@ -0,0 +1,7 @@
+import { join as pathJoin } from "https://deno.land/std@0.208.0/path/join.ts";
+import links from "./links.ts";
+const root: string = pathJoin(Deno.cwd(), "_site");
+await Deno.mkdir(root, { recursive: true });
+for (const [path, url] of links.entries()) {
+	await Deno.writeFile(pathJoin(root, path, "index.html"), new TextEncoder().encode(`<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=${url}"></head><body></body></html>`));
+}
---
+++ links.ts
@@ -0,0 +1,3 @@
+const links: Map<string, string> = new Map<string, string>();
+links.set("form/assistance", "https://forms.gle/AXq9hWq2DsJrDNjP7");
+export default links;
