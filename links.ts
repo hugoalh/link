d4490685eb50c20eb3960017c7af2f32c60100e5
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-14 11:06:29 +0800

    Add link

--- links.ts
+++ links.ts
@@ -1,3 +1,6 @@
 const links: Map<string, string> = new Map<string, string>();
+links.set("", "https://github.com/hugoalh/link");
+links.set("assistance_request", "https://forms.gle/AXq9hWq2DsJrDNjP7");
+links.set("assistance-request", "https://forms.gle/AXq9hWq2DsJrDNjP7");
 links.set("form/assistance", "https://forms.gle/AXq9hWq2DsJrDNjP7");
 export default links;
