0d24af058936c9928241d84e1251b1bb07727932
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-12-27 17:42:55 +0800

    20231227A

--- builder.ts
+++ builder.ts
@@ -1,18 +1,24 @@
 import { dirname as pathDirName } from "std/path/dirname.ts";
 import { join as pathJoin } from "std/path/join.ts";
-import links from "./links.ts";
-const root: string = pathJoin(Deno.cwd(), "_site");
-await Deno.mkdir(root, { recursive: true });
-for (const [url, { description, paths }] of Object.entries(links)) {
+import { parse as yamlParse } from "std/yaml/parse.ts";
+interface LinkEntry {
+	description: string;
+	link: string;
+	paths: string[];
+}
+const siteRoot: string = pathJoin(Deno.cwd(), "_site");
+await Deno.mkdir(siteRoot, { recursive: true });
+const links: LinkEntry[] = yamlParse(await Deno.readTextFile(pathJoin(Deno.cwd(), "links.yml"))) as LinkEntry[];
+for (const { description, link, paths } of links) {
 	for (const pathRelative of paths) {
-		const pathFull: string = pathJoin(root, pathRelative, "index.html");
+		const pathFull: string = pathJoin(siteRoot, pathRelative, "index.html");
 		await Deno.mkdir(pathDirName(pathFull), { recursive: true });
 		await Deno.writeFile(pathFull, new TextEncoder().encode(`<!DOCTYPE html>
 <html lang="en">
 
 <head>
 	<meta charset="utf-8" />
-	<meta http-equiv="refresh" content="5; url=${url}" />
+	<meta http-equiv="refresh" content="5; url=${link}" />
 	<meta name="color-scheme" content="light dark" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<style>
@@ -56,7 +62,7 @@ for (const [url, { description, paths }] of Object.entries(links)) {
 		<div class="address">hxhS (hugoalh & hugoalh Studio) Link Service</div>
 		<div>One of the network service endpoint of hxhS (hugoalh & hugoalh Studio). This endpoint is powered by <a href="https://pages.github.com" hreflang="en" target="_blank">GitHub Pages <sup>↗️</sup></a>.</div>
 	</header>
-	<p>You will be redirected to the ${description} in 5 seconds. <a href="${url}">Please click here if the automatic redirection is not working.</a></p>
+	<p>You will be redirected to the ${description} in 5 seconds. <a href="${link}">Please click here if the automatic redirection is not working.</a></p>
 </body>
 
 </html>`));
